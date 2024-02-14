76e400f56433e2ece56639058b624bac552a23ec
Albert Tseng <albert@cr-a100-80-hn.identity.together.xyz>
2024-02-14 05:36:40 +0000

    remove old stuff

--- model/version.py
+++
@@ -1,9 +0,0 @@
-MODEL_VERSION = 1
-
-
-def check_model_version(test):
-    if test != MODEL_VERSION:
-        raise Exception(
-            f"Saved model version ({test}) does not match the "\
-            f"source code model version ({MODEL_VERSION}). "\
-            "Please pull the latest code or model checkpoints.")
